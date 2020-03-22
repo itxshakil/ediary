88d7e8386568c91a0c88a6ae2ee8c37eeb825c70
Shakil Alam <itxshakiil@gmail.com>
2020-03-22 22:00:51 +0530

    Added Vue,Taileind and purgecss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="bg-gray-300 navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
