2dd3676501ffb46899ff52fa08ad62a4c8723df3
Shakil Alam <itxshakiil@gmail.com>
2020-03-26 12:15:20 +0530

    Added Pages

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public function faq()
+    {
+        return view('pages.faq');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','Login')
+@section('content')
+<div class="container mx-auto flex justify-center items-center h-64 px-3 md:px-6 my-12">
+    <p class="text-4xl tracking-wider text-gray-700">COMING SOON</p>
+</div>
+@endsection
