b8092f5bec487c8876300f2917714acc7814e31d
Shakil Alam <itxshakiil@gmail.com>
2020-03-28 18:04:27 +0530

    added Service worker and minimized

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,15 @@ var urlsToCache = [
     '/',
     '/css/app.css',
     '/js/app.js',
-    '/icons/*.png'
+    '/icons/old/icons-192.png',
+    '/icons/old/icons-24.png',
+    '/icons/old/icons-36.png',
+    '/icons/old/icons-48.png',
+    '/icons/old/icons-72.png',
+    '/icons/old/icons-144.png',
+    '/icons/old/icons-120.png',
+    '/icons/old/icons-512.png',
+    'https://source.unsplash.com/K4mSJ7kc0As/600x800'
 ];
 
 self.addEventListener('install', function (event) {
