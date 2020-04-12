6db59354520dad49fc11e314ac064264edb3555c
Shakil Alam <itxshakiil@gmail.com>
2020-04-12 12:13:26 +0530

    Changed form input name password-confirm => password_confirmation

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,7 +8,8 @@
         </div>
         <div class="w-full lg:w-1/2 bg-gray-100 p-2 md:p-5 rounded-lg lg:rounded-l-none">
             <h3 class="pt-4 text-2xl text-center pb-2 md:pb-4">{{ __('Reset Password') }}</h3>
-            <form class="px-4 md:px-8  pt-6 pb-2 mb-4 bg-white rounded" method="POST" action="{{ route('password.update') }}">
+            <form class="px-4 md:px-8  pt-6 pb-2 mb-4 bg-white rounded" method="POST"
+                action="{{ route('password.update') }}">
                 <input type="hidden" name="token" value="{{ $token }}">
                 <div class="mb-4">
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
@@ -16,8 +17,8 @@
                     </label>
                     <input
                         class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none @error('email') border-red-500 @enderror"
-                        id="email" type="email" placeholder="john@example.com" name="email"  value="{{ $email ?? old('email') }}"
-                        required autocomplete="email" autofocus />
+                        id="email" type="email" placeholder="john@example.com" name="email"
+                        value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus />
                     @error('email')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -34,13 +35,14 @@
                     @enderror
                 </div>
                 <div class="mb-4">
-                    <label class="block mb-2 text-sm font-bold text-gray-700" for="password-confirm">
+                    <label class="block mb-2 text-sm font-bold text-gray-700" for="password_confirmation">
                         {{ __('Confirm Password') }}
                     </label>
                     <input
-                        class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none @error('password-confirm') border-red-500 @enderror"
-                        id="password-confirm" type="password" name="password-confirm" placeholder="******************" />
-                    @error('password-confirm')
+                        class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none @error('password_confirmation') border-red-500 @enderror"
+                        id="password_confirmation" type="password" name="password_confirmation"
+                        placeholder="******************" />
+                    @error('password_confirmation')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
                 </div>
