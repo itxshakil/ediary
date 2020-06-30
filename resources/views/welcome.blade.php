e7a13260d37fc81417f4487dbdcca93b12df64f8
Shakil Alam <itxshakil@gmail.com>
2020-06-30 10:57:35 +0530

    Remove redundant section in confirm password page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <h2 class="text-gray-200 text-2xl hidden md:block md:text-4xl text-center">Securely write and save your private
         Diary</h2>
     <p class="text-gray-200 text-xl md:text-2xl mb-4 text-center">Your diary is your only friend, with whom you can share all your secrets and thoughts.</p>
-    <register-form></register-form>
+    @guest
+        <register-form></register-form>
+    @endguest
 </div>
 @endsection
