c22536185f1cc309f9af0ef6c44dfbf1e690f9e2
Shakil Alam <itxshakil@gmail.com>
2020-07-06 13:00:16 +0530

    Added if else to root div

--- resources/js/components/Diaries.vue
+++ resources/js/components/Diaries.vue
@@ -1,41 +1,39 @@
 <template>
-  <div>
-    <div v-if="addnew" v-cloak>
-      <resizable-textarea>
-        <textarea
-          class="notebook w-full px-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none"
-          name="entry"
-          id="entry"
-          cols="30"
-          rows="10"
-          placeholder="Its was an awesome day today."
-          required
-          autofocus
-          v-model="entry"
-        ></textarea>
-      </resizable-textarea>
-      <button
-        class="mt-4 bg-blue-100 active:bg-blue-200 text-blue-800 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
-        @click="save"
-      >Save</button>
-      <button
-        class="mt-4 bg-gray-600 text-gray-100 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
-        @click="addnew=false"
-      >Cancel</button>
+  <div v-if="addnew">
+    <resizable-textarea>
+      <textarea
+        class="notebook w-full px-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none"
+        name="entry"
+        id="entry"
+        cols="30"
+        rows="10"
+        placeholder="It was an awesome day."
+        required
+        autofocus
+        v-model="entry"
+      ></textarea>
+    </resizable-textarea>
+    <button
+      class="mt-4 bg-blue-100 active:bg-blue-200 text-blue-800 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
+      @click="save"
+    >Save</button>
+    <button
+      class="mt-4 bg-gray-600 text-gray-100 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
+      @click="addnew=false"
+    >Cancel</button>
+  </div>
+  <div v-else>
+    <div class="entries flex flex-wrap items-stretch">
+      <diary v-for="diary in items" :key="diary.id" :data="diary"></diary>
     </div>
-    <div v-else>
-      <div class="entries flex flex-wrap items-stretch">
-        <diary v-for="diary in items" :key="diary.id" :data="diary"></diary>
-      </div>
-      <paginator :dataSet="dataSet" @changed="fetch" v-cloak></paginator>
-      <div class="fixed right-0 bottom-0 mb-6 mr-6">
-        <share class="md:hidden mb-2 p-4 rounded-full w-full bg-blue-200 shadow hover:shadow-lg"></share>
-        <div
-          class="p-4 md:pb-6 rounded-full text-bold text-4xl h-12 w-12 bg-blue-200 text-blue-800 shadow hover:shadow-lg flex justify-center items-center"
-          title="Add new"
-          @click="addnew=true"
-        >+</div>
-      </div>
+    <paginator :dataSet="dataSet" @changed="fetch" v-cloak></paginator>
+    <div class="fixed right-0 bottom-0 mb-6 mr-6">
+      <share class="md:hidden mb-2 p-4 rounded-full w-full bg-blue-200 shadow hover:shadow-lg"></share>
+      <div
+        class="p-4 md:pb-6 rounded-full text-bold text-4xl h-12 w-12 bg-blue-200 text-blue-800 shadow hover:shadow-lg flex justify-center items-center"
+        title="Add new"
+        @click="addnew=true"
+      >+</div>
     </div>
   </div>
 </template>
