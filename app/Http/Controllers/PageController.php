8cfb52322a3259ec25b5fdaaa51cd9078e5ab4c0
Shakil Alam <itxshakil@gmail.com>
2020-07-07 09:05:52 +0530

    Added validation for username to be alpha numeric with dash

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
     public function send(Request $request)
     {
         $request->validate([
-            'name' => ['required'],
+            'name' => ['required', 'alpha' , 'max:200'],
             'email' => ['required', 'email'],
             'message' => ['required', 'string']
         ]);
