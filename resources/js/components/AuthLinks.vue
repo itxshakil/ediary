319816b2e88b87e20b73f908ab62f12114c5af07
Shakil Alam <itxshakil@gmail.com>
2020-07-09 17:16:47 +0530

    Added If-else to root div in AuthLinks.vue

--- resources/js/components/AuthLinks.vue
+++ resources/js/components/AuthLinks.vue
@@ -1,48 +1,46 @@
 <template>
-  <div>
-    <div v-if="!isloggedin" class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-      <a
-        class="hidden md:block bg-blue-100 active:bg-blue-200 text-blue-800 px-4 py-2 rounded outline-none focus:outline-none mr-2 uppercase shadow hover:shadow-md font-bold text-xs"
-        href="/register"
-      >Register</a>
-      <a
-        class="bg-gray-700 text-gray-100 px-4 py-2 rounded outline-none focus:outline-none mr-2 uppercase shadow hover:shadow-md font-bold text-xs"
-        href="/login"
-      >Login</a>
-    </div>
-    <dropdown v-cloak v-else>
-      <img
-        slot="toggler"
-        class="flex text-sm border-2 focus:outline-none focus:border-white transition duration-150 ease-in-out text-gray-100 w-10 rounded-full"
-        :src="user.profile.image"
-        :alt="user.username"
-      />
-
-      <span slot="items" class="flex flex-col py-1 rounded-md bg-white shadow-xs">
-        <a
-          v-if="!isEmailVerified"
-          href="/email/verify"
-          class="block px-4 py-2 text-sm leading-5 text-red-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-        >Verify Email Address</a>
-        <a
-          :href="'/user/' + user.username"
-          class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-        >My Profile</a>
-        <a
-          href="/home"
-          class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-        >Dashboard</a>
-        <a
-          href="/password/change"
-          class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-        >Change Password</a>
-        <logout-button
-          @loggedout="logout"
-          class="block px-4 text-sm py-2 leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-        ></logout-button>
-      </span>
-    </dropdown>
+  <div v-if="!isloggedin" class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+    <a
+      class="hidden md:block bg-blue-100 active:bg-blue-200 text-blue-800 px-4 py-2 rounded outline-none focus:outline-none mr-2 uppercase shadow hover:shadow-md font-bold text-xs"
+      href="/register"
+    >Register</a>
+    <a
+      class="bg-gray-700 text-gray-100 px-4 py-2 rounded outline-none focus:outline-none mr-2 uppercase shadow hover:shadow-md font-bold text-xs"
+      href="/login"
+    >Login</a>
   </div>
+  <dropdown v-cloak v-else>
+    <img
+      slot="toggler"
+      class="flex text-sm border-2 focus:outline-none focus:border-white transition duration-150 ease-in-out text-gray-100 w-10 rounded-full"
+      :src="user.profile.image"
+      :alt="user.username"
+    />
+
+    <span slot="items" class="flex flex-col py-1 rounded-md bg-white shadow-xs">
+      <a
+        v-if="!isEmailVerified"
+        href="/email/verify"
+        class="block px-4 py-2 text-sm leading-5 text-red-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      >Verify Email Address</a>
+      <a
+        :href="'/user/' + user.username"
+        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      >My Profile</a>
+      <a
+        href="/home"
+        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      >Dashboard</a>
+      <a
+        href="/password/change"
+        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      >Change Password</a>
+      <logout-button
+        @loggedout="logout"
+        class="block px-4 text-sm py-2 leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      ></logout-button>
+    </span>
+  </dropdown>
 </template>
 <script>
 import logoutButton from "./LogoutButton";
