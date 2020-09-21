0afae1b2de916845a840fb1f9e1b61d0374a4e19
Shakil Alam <itxshakil@gmail.com>
2020-09-21 16:20:28 +0530

    Remove custom result count search

--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <section class="container max-w-screen-md mx-auto text-gray-900 p-4">
     <h1 class="font-semibold text-2xl">Search Result for {{request()->input('q')}}</h1>
-    <p class="mb-2">{{$users->total()}} results for {{request()->input('q')}}</p>
 
     @forelse ($users as $user)
     @include('search._user')
@@ -14,7 +13,7 @@
     @endforelse
 
     @if ($users->count() > 0)
-    {{$users->appends(request()->input())->links()}}
+    {{$users->withQueryString()->links()}}
     @endif
 </section>
 @endsection
