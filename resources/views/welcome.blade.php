3f55f5fed8e421fe882da80cc9141e1a0ba0592f
Shakil Alam <itxshakil@gmail.com>
2021-01-25 17:38:25 +0530

    Update route to its name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
     <h1 class="text-2xl underline p-4 font-bold">Latest Articles</h1>
     <div class="flex flex-col md:flex-row gap-4 p-4">
         <div class="w-full h-auto bg-gray-400 text-gray-900 lg:block lg:w-1/2 bg-cover rounded-lg p-4 m-1">
-            <h2 class="font-bold text-lg"><a href="/blogs/how-to-write-diary">How to write a Diary</a></h2>
+            <h2 class="font-bold text-lg"><a href="{{route('blogs.how-to-write')}}">How to write a Diary</a></h2>
             <cite class="uppercase text-xs font-bold">Written By</cite> <a class="uppercase text-xs font-bold"
                 href="https://www.wikihow.com/Write-a-Diary">WikiHow</a>
             <p>Diaries are wonderful objects that allow you to discuss your emotions, record dreams or ideas, and
@@ -83,12 +83,12 @@
                 some
                 basic tricks you can use to get the most out of your writing. If you aren't sure what to write about,
                 using
-                prompts like inspirational quotes can help get started on new entries. <a href="/blogs/how-to-write-diary"
+                prompts like inspirational quotes can help get started on new entries. <a href="{{route('blogs.how-to-write')}}"
                     class="text-blue-600">read more...</a></p>
         </div>
         <div class="w-full h-auto bg-gray-400 text-gray-900 lg:block lg:w-1/2 bg-cover rounded-lg p-4 m-1">
             <h2 class="font-bold text-lg"><a
-                    href="/blogs/these-8-good-things-will-happen-when-you-start-writing-diary">These 8
+                    href="{{route('blogs.these-8-good-things')}}">These 8
                     Good Things Will
                     Happen When You Start Writing Diaries</a></h2>
             <cite class="uppercase text-xs font-bold">Written By</cite> <a class="uppercase text-xs font-bold"
@@ -99,22 +99,22 @@
                 life can be healing and teach you to know yourself better. It can also unlock the power of your
                 creativity,
                 and inspire you to manifest dreams <a
-                    href="/blogs/these-8-good-things-will-happen-when-you-start-writing-diary" class="text-blue-600">read
+                    href="{{route('blogs.these-8-good-things')}}" class="text-blue-600">read
                     more...</a></p>
         </div>
         <div class="w-full h-auto bg-gray-400 text-gray-900 lg:block lg:w-1/2 bg-cover rounded-lg p-4 m-1">
-            <h2 class="font-bold text-lg"><a href="/blogs/how-to-start-writing-a-diary">How to Start Writing a Diary</a>
+            <h2 class="font-bold text-lg"><a href="{{route('blogs.how-to-start-writing-a-diary')}}">How to Start Writing a Diary</a>
             </h2>
             <cite class="uppercase text-xs font-bold">Written By</cite> <a class="uppercase text-xs font-bold"
                 href="#">INTERNET
                 BLOGGER</a>
             <p>To start a diary, all you need is a willingness to write. Start by figuring out what you want
                 to write in your journal.
-                If you aren’t sure, simply start writing and see where that leads. It can also be useful to set a time
+                If you are not sure, simply start writing and see where that leads. It can also be useful to set a time
                 limit
                 in your
                 early writing sessions. Set an alarm for 10 to 20 minutes and start writing. <a
-                    href="/blogs/how-to-start-writing-a-diary" class="text-blue-600">read
+                    href="{{route('blogs.how-to-start-writing-a-diary')}}" class="text-blue-600">read
                     more...</a></p>
         </div>
     </div>
