dd524f79be7867096975b2012c9ec43ca6b6315a
Shakil Alam <itxshakil@gmail.com>
2021-01-25 19:02:29 +0530

    Update API/Controller

--- app/Http/Controllers/Api/DiaryController.php
+++ app/Http/Controllers/Api/DiaryController.php
@@ -2,40 +2,46 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Diary;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class DiaryController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
      */
     public function index()
     {
-        $diaries = auth()->user()->diaries()->latest()->paginate(12);
-
-        return $diaries;
+        return auth()->user()->diaries()->latest()->paginate(12);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Response
+     * @return Diary
      */
-    public function store(Request $request)
+    public function store(Request $request):Diary
     {
         $validatedData = $request->validate(['entry' => ['required', 'string']]);
 
         $diary = auth()->user()->diaries()->create($validatedData);
 
+        $this->addCreatedAtIfAvailable($request, $diary);
+
+        return $diary;
+    }
+
+    /**
+     * @param Request $request
+     * @param $diary
+     */
+    protected function addCreatedAtIfAvailable(Request $request, $diary): void
+    {
         if ($request->filled('created_at')) {
             $diary->created_at = $request->created_at;
             $diary->save();
         }
-
-        return $diary;
     }
 }
