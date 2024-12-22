e917310f8d5b92b301c0fbf0c02f90bdc112003a
Shakil Alam <itxshakil@gmail.com>
2024-12-22 12:54:18 +0530

    Add h1 to about and success
    Signed-off-by: Shakil Alam <itxshakil@gmail.com>

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,6 +2,6 @@
 @section('title','About E-diary App')
 @section('content')
 <div class="container mx-auto flex justify-center items-center h-64 px-3 md:px-6">
-    <p class="text-4xl tracking-wider text-gray-700">COMING SOON</p>
+    <h1 class="text-4xl tracking-wider text-gray-700">COMING SOON</h1>
 </div>
 @endsection
--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container mx-auto flex justify-center px-3 md:px-6">
     <div class="w-full lg:w-1/2 bg-gray-200 dark:bg-gray-800 dark:text-white p-2 md:p-5 rounded-lg lg:rounded-l-none py-6">
-        <h3 class="pt-4 text-2xl text-center pb-2 md:pb-4">Your message sent successfully!</h3>
-        <p class="mt-4 text-center">We will reply your as soon as possible</p>
+        <h1 class="pt-4 text-2xl text-center pb-2 md:pb-4">Your message sent successfully!</h1>
+        <h3 class="mt-4 text-center">We will reh3ly your as soon as possible</h3>
     </div>
 </div>
 @endsection
