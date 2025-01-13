de33e23f45f00b7d901e92b586a7d71df57498c5
Shakil Alam <itxshakil@gmail.com>
2025-01-13 22:18:10 +0530

    feat: More

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,12 @@
 @section('content')
 <div class="flex flex-col px-3 md:px-6 bg-blue-700 text-gray-200">
     <div class="flex flex-col justify-center items-center min-h-screen max-w-4xl mx-auto">
-        <h1 class="text-4xl max-w-3xl sm:block md:text-5xl text-center text-pretty">Capture Your Life’s Journey: Secure, Private, and Effortless</h1>
-        <h2 class="text-xl md:text-2xl mb-4 text-center mt-2">
-            <strong>Struggling to keep track of your thoughts and memories?</strong> Start documenting your life with Ediary—a safe haven for your innermost reflections, accessible anytime, anywhere.
-        </h2>
+        <div class="min-h-screen flex flex-col items-center justify-center">
+            <h1 class="text-4xl max-w-3xl sm:block md:text-5xl text-center text-pretty">Capture Your Life’s Journey: Secure, Private <span class="hidden sm:inline">, and Effortless</span></h1>
+            <h2 class="text-xl md:text-2xl mb-4 text-center mt-2">
+                <strong>Struggling to keep track of your thoughts and memories?</strong> Start documenting your life with Ediary—a safe haven for your innermost reflections, accessible anytime, anywhere.
+            </h2>
+        </div>
         @guest
         <register-form></register-form>
         @endguest
