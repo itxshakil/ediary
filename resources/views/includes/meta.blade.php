2d366da254af9b5de7643f4e0111691b473f73e6
Shakil Alam <itxshakil@gmail.com>
2025-01-13 21:10:01 +0530

    Update meta.blade.php

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -1,18 +1,18 @@
 <meta name="description"
-    content="E-diary is a free service to keep track of your past and think about your future. You can make predictions about what will happen and see if they come true. TRY NOW FOR FREE!!!" />
+    content="Are you losing track of precious moments or struggling to organize your thoughts? With Ediary, enjoy ultimate privacy, offline access, and secure storage for your deepest reflections. Start your free journey today—because your memories matter!" />
 <meta name="keywords"
     content="ediary,ediary app, private diary, secure diary,privacy friendly , diary App , save diary , free ediary offline , pwa, shakil alam" />
-<meta name="subject" content="Securely write and save your private Diary | Ediary" />
+<meta name="subject" content="Ediary - Securely Document Your Life, Free & Effortless" />
 <meta name="language" content="en" />
 <meta name="rating" content="General" />
 <meta name="url" content="https://ediary.shakiltech.com/" />
 <meta name="identifier-URL" content="https://ediary.shakiltech.com/" />
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:site" content="@itxshakil" />
-<meta name="twitter:title" content="Securely write and save your private Diary" />
+<meta name="twitter:title" content="Ediary - Securely Document Your Life, Free & Effortless" />
 <meta name="twitter:creator" content="@itxshakil" />
-<meta name="og:title" content="Securely write and save your private Diary" />
+<meta name="og:title" content="Ediary - Securely Document Your Life, Free & Effortless" />
 <meta name="og:url" content="https://ediary.shakiltech.com/" />
 <meta name="og:image" content="{{ url('/icons/apple-icon-96x96.png') }}" />
 <meta name="og:description"
-    content="E-diary is privacy friendly e-diary Website that securely save your diary. TRY NOW FOR FREE!!!" />
+    content="Are you losing track of precious moments or struggling to organize your thoughts? With Ediary, enjoy ultimate privacy, offline access, and secure storage for your deepest reflections. Start your free journey today—because your memories matter!" />
